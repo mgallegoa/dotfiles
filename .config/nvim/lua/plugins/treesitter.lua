5c35d54c3a9c5fd36577cf1867674c04c93c6f82
mgallegoa <mgallegoarias@gmail.com>
2024-08-17 16:49:01 -0500

    Modify config to automate recognize the language
    Change the linit to auto_install = true to automate recognize and install the language detected by treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,14 @@
 return {
-   "nvim-treesitter/nvim-treesitter", 
-   run= ":TSUpdate",
-   config = function ()
-   local configs = require("nvim-treesitter.configs")
-     configs.setup({
-     ensure_installed = { "lua", "vim", "javascript", "html" },
-        highlight = { enable = true },
-        indent = { enable = true },  
-     })
+  "nvim-treesitter/nvim-treesitter",
+  run = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+    --Replaced the next line to automate recognize the languaje
+    --ensure_installed = { "lua", "vim", "javascript", "html" },
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
   end
 }
