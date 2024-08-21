5ff8b7fb4047e16148e6a2a8267261f99a03db00
mgallegoa <manuelgallego2003@yahoo.com>
2024-08-21 14:07:23 -0500

    Add nvim tmux navigation
    Add the plugin nvim-tmux-navigator.lua to navigate from nvim to tmux panels.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,7 +14,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   end
 end
 vim.opt.rtp:prepend(lazypath)
-
 require("vim-customs")
 require('lazy').setup("plugins")
 
--- .config/nvim/lua/vim-customs.lua
+++ .config/nvim/lua/vim-customs.lua
@@ -6,10 +6,21 @@ vim.cmd("noremap ii <Esc>")
 vim.cmd("inoremap ii <Esc>")
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.wo.relativenumber = true
 vim.wo.number = true
 vim.o.scrolloff = 10
+
+-- Copy the entire line to the clipboard
+vim.keymap.set('n', '<leader>Y', '"+yy', { noremap = true, silent = true })
+-- Copy the selected text to the clipboard
+vim.keymap.set('v', '<leader>y', '"+y', { noremap = true, silent = true })
+
+-- Move the pane (split), in an ease way
+vim.keymap.set('n', '<leader>h', '<C-w>h', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true, silent = true })
+
