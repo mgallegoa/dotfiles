e2aef1421368edf5971b1aae06a813d2581d30f4
mgallegoa <manuelgallego2003@yahoo.com>
2024-08-25 12:08:11 -0500

    Fix a error with eslint_d for eslint server use
    This fix the movement descrived here https://github.com/nvimtools/none-ls.nvim/discussions/81, the fix was described here https://stackoverflow.com/questions/78150730/failed-to-load-builtin-eslint-d-for-method-diagnostics

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,14 +1,24 @@
 return {
   "nvimtools/none-ls.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    -- Upgrade to fix the error https://github.com/nvimtools/none-ls.nvim/discussions/81
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
 
       source = {
+        -- LUA
         null_ls.builtins.formatting.stylua,
+
+        ------------------------------
+        --- JS | TS | HTML | CSS and more...
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint_d,
+        -- Upgrade to fix the error https://github.com/nvimtools/none-ls.nvim/discussions/81
+        require("none-ls.diagnostics.eslint_d"),
+        ------------------------------
       }
     })
     vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
