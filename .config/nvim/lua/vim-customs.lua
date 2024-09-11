1755d56dee3b79dd9e74588a2a52a4363a30086d
mgallegoa <manuelgallego2003@yahoo.com>
2024-09-11 13:22:15 -0500

    Add keybanding for recize the nvim window, split windows and move tabs
    Useful keybandings for recize the nvim windows and move between tabs

--- .config/nvim/lua/vim-customs.lua
+++ .config/nvim/lua/vim-customs.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set signcolumn=yes")
 vim.cmd("noremap ff <Esc>")
 vim.cmd("inoremap ff <Esc>")
 
@@ -32,3 +33,18 @@ vim.keymap.set('n', '<leader>h', '<C-w>h', { noremap = true, silent = true, desc
 vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true, silent = true, desc = "Move pane to right (l)" })
 vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true, desc = "Move pane to up (k)" })
 vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true, silent = true, desc = "Move pane to down (j)" })
+
+-- Easy for recize the nvim panes
+vim.keymap.set("n", "<M-H>", ':wincmd h<CR>', { noremap = true, silent = true, desc = "Move pane to left (h)" })
+vim.keymap.set("n", "<M-J>", ':wincmd j<CR>', { noremap = true, silent = true, desc = "Move pane to down (j)" })
+vim.keymap.set("n", "<M-K>", ':wincmd k<CR>', { noremap = true, silent = true, desc = "Move pane to up (k)" })
+vim.keymap.set("n", "<M-L>", ':wincmd l<CR>', { noremap = true, silent = true, desc = "Move pane to right (l)" })
+
+-- Split windows
+vim.keymap.set("n", "ss", ":vsplit<Return>", { noremap = true, silent = true, desc = "Create a vsplit window" })
+vim.keymap.set("n", "sv", ":split<Return>", { noremap = true, silent = true, desc = "Create a hsplit window" })
+
+-- Tabs
+vim.keymap.set("n", "te", ":tabedit", { noremap = true, silent = true, desc = "Create a new vim tab" })
+vim.keymap.set("n", "<tab>", ":tabnext<Return>", { noremap = true, silent = true, desc = "Move to next tab" })
+vim.keymap.set("n", "<s-tab>", ":tabprev<Return>", { noremap = true, silent = true, desc = "Move to previous tab" })
