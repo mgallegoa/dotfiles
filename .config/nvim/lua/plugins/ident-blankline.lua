03345f09df02984a498995597f1a99686381fa8b
mgallegoa <manuelgallego2003@yahoo.com>
2024-09-12 16:00:35 -0500

    Add plugin to ident the blanck lines for better visual of the code

--- .config/nvim/lua/plugins/ident-blankline.lua
+++ .config/nvim/lua/plugins/ident-blankline.lua
@@ -18,13 +18,13 @@ return {
     -- create the highlight groups in the highlight setup hook, so they are reset
     -- every time the colorscheme changes
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#EDABB0" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#EED6AA" })
+      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#A2D0F6" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#E8CBB0" })
+      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#C9DFB9" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#DCADEB" })
+      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#B4DEE4" })
     end)
 
     require("ibl").setup { indent = { highlight = highlight } }
