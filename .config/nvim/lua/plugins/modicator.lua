e3114eaa077af877da72404524b6babfb28fbb7a
mgallegoa <manuelgallego2003@yahoo.com>
2024-09-13 18:10:09 -0500

    Add plugin modicator to higlight the line number, integrated with lualine
    Plugin modicator higlight the number at the left of the nvim, this plugin have integration with lualine out of the box

---
+++ .config/nvim/lua/plugins/modicator.lua
@@ -0,0 +1,14 @@
+-- TODO: This modicator (modify indicator) is for highlight the line number https://github.com/mawkler/modicator.nvim
+return {
+  'mawkler/modicator.nvim',
+  dependencies = "catppuccin/nvim", -- Theme here
+  init = function()
+    -- These are required for Modicator to work
+    vim.o.cursorline = true
+    vim.o.number = true
+    vim.o.termguicolors = true
+  end,
+  config = function()
+    require('modicator').setup()
+  end
+}
