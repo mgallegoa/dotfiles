c18d72dcf1b48f11be4d0d52a4e110b3fd2cfc97
mgallegoa <manuelgallego2003@yahoo.com>
2024-09-22 11:32:50 -0500

    Change the keybanding, collision with other keysbanding
    The <leader>l collisioned with pane jump to right

--- .config/nvim/lua/plugins/linters.lua
+++ .config/nvim/lua/plugins/linters.lua
@@ -22,6 +22,6 @@ return {
       end,
     })
 
-    vim.keymap.set("n", "<leader>l", function() lint.try_lint() end, { desc = "Trigger linting for current file" })
+    vim.keymap.set("n", "<leader>gl", function() lint.try_lint() end, { desc = "Trigger linting for current file" })
   end,
 }
