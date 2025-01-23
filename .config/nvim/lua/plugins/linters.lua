c62bed807f31778a1bc64d3eed0b760c705b01d6
mgallegoa <manuelgallego2003@yahoo.com>
2025-01-23 16:43:26 -0500

    Nvim: Linter, query the root_dir of the lsp clients, and set the cwd based on that for the linter

--- .config/nvim/lua/plugins/linters.lua
+++ .config/nvim/lua/plugins/linters.lua
@@ -13,18 +13,20 @@ return {
 			htmlhint = { "htmlhint" },
 			python = { "pylint" },
 		}
-
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		-- https://github.com/mfussenegger/nvim-lint/issues/482
+		local get_clients = vim.lsp.get_clients or vim.lsp.get_active_clients
+		local client = get_clients({ bufnr = 0 })[1] or {}
 
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
-				lint.try_lint()
+				lint.try_lint(nil, { cwd = client.root_dir })
 			end,
 		})
 
 		vim.keymap.set("n", "<leader>gl", function()
-			lint.try_lint()
+			lint.try_lint(nil, { cwd = client.root_dir })
 		end, { desc = "Trigger linting for current file" })
 	end,
 }
