4b7f7998d008f419b4b2b469575d90692a91f510
mgallegoa <manuelgallego2003@yahoo.com>
2025-04-04 17:04:49 -0500

    Dev Container : Edit logs append for bash script to configure Tmux.

--- devContainer/setup-tmux.sh
+++ devContainer/setup-tmux.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "TMUX : creating simlink to config dot file" | tee $HOME/setup.log
+echo "TMUX : creating simlink to config dot file" | tee -a $HOME/setup.log
 ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
 
-echo "TMUX : cloning the tmux plugin manager" | tee $HOME/setup.log
+echo "TMUX : cloning the tmux plugin manager" | tee -a $HOME/setup.log
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
