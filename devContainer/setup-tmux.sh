e04a1b6806f250a953a857e3ed5940909ff0c55c
mgallegoa <manuelgallego2003@yahoo.com>
2025-04-04 21:55:35 -0500

    Dev Container : Fix logs append for bash script to configure Tmux.

--- devContainer/setup-tmux.sh
+++ devContainer/setup-tmux.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "TMUX : creating simlink to config dot file" | tee -a $HOME/setup.log
-ln -sf "$PWD/.tmux.conf" "$HOME"/.tmux.conf
+declare -x PATH_DOTFILES="$HOME/dotfiles"
 
-echo "TMUX : cloning the tmux plugin manager" | tee -a $HOME/setup.log
+echo "TMUX : Creating simlink to config dot file." | tee -a $HOME/setup.log
+ln -sf $PATH_DOTFILES/.tmux.conf $HOME/.tmux.conf
+
+echo "TMUX : Cloning the tmux plugin manager." | tee -a $HOME/setup.log
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-echo "TMUX : Finished" | tee -a $HOME/setup.log
+echo "TMUX : Custon configuration finished." | tee -a $HOME/setup.log
