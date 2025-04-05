e77f50bce6526bb12758e771e543e95caa016489
mgallegoa <manuelgallego2003@yahoo.com>
2025-04-05 13:50:39 -0500

    Dev Container : Eddit bash script to configure Tmux.

--- devContainer/setup-tmux.sh
+++ devContainer/setup-tmux.sh
@@ -5,17 +5,16 @@ declare -x PATH_DOTFILES="$HOME/dotfiles"
 echo "TMUX : Creating simlink to config dot file." | tee -a $HOME/setup.log
 ln -sf $PATH_DOTFILES/.tmux.conf $HOME/.tmux.conf
 
-# Name of the tmux session
-# TMUX_SESSION="my-session"
-# # Start a new session if not already running
-# tmux has-session -t $TMUX_SESSION 2>/dev/null
-# if [ $? != 0 ]; then
-#   tmux new-session -d -s $TMUX_SESSION
-# fi
-#
+TMUX_SESSION_NAME="tmux_configuration"
+# Start a new session if not already running
+tmux has-session -t $TMUX_SESSION_NAME 2>/dev/null
+if [ $? != 0 ]; then
+  tmux new-session -A -s $TMUX_SESSION_NAME
+fi
+
 echo "TMUX : Source to tmux configuration file." | tee -a $HOME/setup.log
-# tmux send-keys -t $TMUX_SESSION "source-file $HOME/.tmux.conf" C-m
-tmux send-keys "source-file $HOME/.tmux.conf" C-m
+tmux send-keys -t $TMUX_SESSION_NAME "source-file $HOME/.tmux.conf" C-m
+#tmux send-keys "source-file $HOME/.tmux.conf" C-m
 
 echo "TMUX : Cloning the tmux plugin manager." | tee -a $HOME/setup.log
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
