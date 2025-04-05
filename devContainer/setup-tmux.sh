98b1d69ab09510c6f12bd23778cc49acfd947dc6
mgallegoa <manuelgallego2003@yahoo.com>
2025-04-05 14:48:45 -0500

    Dev Container : Eddit bash script to configure Tmux.

--- devContainer/setup-tmux.sh
+++ devContainer/setup-tmux.sh
@@ -13,13 +13,13 @@ if [ $? != 0 ]; then
 fi
 
 echo "TMUX : Source to tmux configuration file." | tee -a $HOME/setup.log
-tmux send-keys -t $TMUX_SESSION_NAME "source-file $HOME/.tmux.conf" C-m
+tmux send-keys -t $TMUX_SESSION_NAME "tmux source-file $HOME/.tmux.conf" C-m
 #tmux send-keys "source-file $HOME/.tmux.conf" C-m
 
 echo "TMUX : Cloning the tmux plugin manager." | tee -a $HOME/setup.log
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
 echo "TMUX : Install plugins." | tee -a $HOME/setup.log
-tmux send-keys -t $TMUX_SESSION_NAME "C-b I" C-m
+$HOME/.tmux/plugins/tpm/bin/install_plugins
 
 echo "TMUX : Custon configuration finished." | tee -a $HOME/setup.log
