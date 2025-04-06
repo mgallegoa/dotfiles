7385b68396244cd1c8eac3a20c57f8a61e382f1d
mgallegoa <manuelgallego2003@yahoo.com>
2025-04-06 11:01:57 -0500

    Dev Container : Fix script to install MesloLGS Nerd Fonds.

--- devContainer/setup-nerdFonts.sh
+++ devContainer/setup-nerdFonts.sh
@@ -6,24 +6,24 @@ set -euo pipefail
 # FONT_NAME="MesloLGS"
 FONT_NAME="Meslo"
 FONT_VARIANT="Nerd Font Mono"
-FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/${FONT_NAME}.tar.xz"
+FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/$FONT_NAME.tar.xz"
 declare -x INSTALL_DIR="$HOME/.local/share/fonts"
 
 mkdir -p "$INSTALL_DIR"
 
 echo "NERD-FONTS : Downloading $FONT_NAME..." | tee -a $HOME/setup.log
-curl -fLo "{$FONT_NAME}.tar.xz" $FONT_URL
-TAR_FILE=$(tar -l "{$FONT_NAME}.tar.xz" | awk '{print $4}' | grep '\.tar\.xz$')
+curl -fLo "$HOME/$FONT_NAME.tar.xz" $FONT_URL
+TAR_FILE=$(tar -l "$HOME/$FONT_NAME.tar.xz" | awk '{print $4}' | grep '\.tar\.xz$')
 
 # Unzip to font directory
 echo "NERD-FONTS : Installing $FONT_NAME $FONT_VARIANT to $INSTALL_DIR" | tee -a $HOME/setup.log
-tar -xf "{$FONT_NAME}.tar.xz" -C "$INSTALL_DIR"
+tar -xf "$HOME/$FONT_NAME.tar.xz" -C $INSTALL_DIR
 
 # Clean up
-rm "{$FONT_NAME}.tar.xz"
+rm "$HOME/$FONT_NAME.tar.xz"
 
 # Refresh font cache
 echo "NERD-FONTS : Refreshing font cache..." | tee -a $HOME/setup.log
 fc-cache -fv "$INSTALL_DIR"
 
-echo "NERD-FONTS : ${FONT_NAME} ${FONT_VARIANT} installed!." | tee -a $HOME/setup.log
+echo "NERD-FONTS : $FONT_NAME $FONT_VARIANT installed!." | tee -a $HOME/setup.log
