eac171ba32ef7eee3f677e394b461962f48ec3db
mgallegoa <manuelgallego2003@yahoo.com>
2025-04-08 16:27:47 -0500

    Dev Container : Fix use Java source and install Java 21.0.6-tem urin.

--- devContainer/setup-java.sh
+++ devContainer/setup-java.sh
@@ -9,7 +9,12 @@ export SDKMAN_DIR="$PATH_INSTALL_OPT/sdkman"
 
 echo "*** JAVA : SDKMan, downloading installer in $SDKMAN_DIR." | tee -a $HOME/setup.log
 curl -s "https://get.sdkman.io" | bash
+
 echo "JAVA : SDKMan, source to $SDKMAN_DIR." | tee -a $HOME/setup.log
-source "$SDKMAN_DIR/bin/sdkman-init.sh"
+export SDKMAN_DIR="/opt/manuel/sdkman"
+ [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+
+echo "JAVA : SDKMan, Installing OpenJdk java 21.0.6-tem usin the SDKMan." | tee -a $HOME/setup.log
+sdk install java 21.0.6-tem
 
 echo "*** JAVA : SDKMan, Custon configuration finished." | tee -a $HOME/setup.log
