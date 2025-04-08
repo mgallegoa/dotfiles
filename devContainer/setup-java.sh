947a332b4f45adeb514d37c176ead8698d9c749f
mgallegoa <manuelgallego2003@yahoo.com>
2025-04-08 16:17:05 -0500

    Dev Container : Fix use Java source to opt/manuel/sdkman to install Java.

--- devContainer/setup-java.sh
+++ devContainer/setup-java.sh
@@ -9,6 +9,7 @@ export SDKMAN_DIR="$PATH_INSTALL_OPT/sdkman"
 
 echo "*** JAVA : SDKMan, downloading installer in $SDKMAN_DIR." | tee -a $HOME/setup.log
 curl -s "https://get.sdkman.io" | bash
+echo "JAVA : SDKMan, source to $SDKMAN_DIR." | tee -a $HOME/setup.log
 source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
 echo "*** JAVA : SDKMan, Custon configuration finished." | tee -a $HOME/setup.log
