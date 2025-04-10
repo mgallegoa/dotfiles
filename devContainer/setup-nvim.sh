6b24d26462dd912b14d2aab6cee8ea048e2e90be
mgallegoa <manuelgallego2003@yahoo.com>
2025-04-10 13:10:55 -0500

    Dev Container : Nvim configure the version of nvim to install.

--- devContainer/setup-nvim.sh
+++ devContainer/setup-nvim.sh
@@ -5,20 +5,21 @@ set -euo pipefail
 
 declare -x PATH_DOTFILES="$HOME/dotfiles"
 declare -x PATH_INSTALL_OPT="/opt/manuel"
+declare -x NVIM_VERSION="v0.10.2"
 
 echo "*** NVIM: downloading prebuild nvim (not source code)." | tee -a $HOME/setup.log
-curl -Lo $PATH_INSTALL_OPT/nvim-linux64-v0.10.2.tar.gz https://github.com/neovim/neovim/releases/download/v0.10.2/nvim-linux64.tar.gz
-echo "NVIM: Unzip in folder $PATH_INSTALL_OPT/nvim-linux64-v0.10.2" | tee -a $HOME/setup.log
-tar -xzf $PATH_INSTALL_OPT/nvim-linux64-v0.10.2.tar.gz -C $PATH_INSTALL_OPT/
-mv $PATH_INSTALL_OPT/nvim-linux64 $PATH_INSTALL_OPT/nvim-linux64-v0.10.2
-rm $PATH_INSTALL_OPT/nvim-linux64-v0.10.2.tar.gz
+curl -Lo "$PATH_INSTALL_OPT/nvim-linux64-$NVIM_VERSION.tar.gz" https://github.com/neovim/neovim/releases/download/$NVIM_VERSION/nvim-linux64.tar.gz
+echo "NVIM: Unzip in folder $PATH_INSTALL_OPT/nvim-linux64-$NVIM_VERSION" | tee -a $HOME/setup.log
+tar -xzf "$PATH_INSTALL_OPT/nvim-linux64-$NVIM_VERSION.tar.gz" -C $PATH_INSTALL_OPT/
+mv $PATH_INSTALL_OPT/nvim-linux64 "$PATH_INSTALL_OPT/nvim-linux64-$NVIM_VERSION"
+rm "$PATH_INSTALL_OPT/nvim-linux64-$NVIM_VERSION.tar.gz"
 echo "NVIM: Creating simlink to config dot files" | tee -a $HOME/setup.log
 mkdir -p "$HOME/.config"
 ln -sf $PATH_DOTFILES/.config/nvim $HOME/.config/
-sudo ln -s $PATH_DOTFILES/nvim-linux64-v0.10.2/bin/nvim /usr/bin/
+sudo ln -s "$PATH_DOTFILES/nvim-linux64-$NVIM_VERSION/bin/nvim" /usr/bin/
 
 echo "NVIM: Run nvim in headless mode to sync Lazy." | tee -a $HOME/setup.log
-# nvim --headless "+Lazy! sync" +qa
+"$PATH_DOTFILES/nvim-linux64-$NVIM_VERSION/bin/nvim" --headless "+Lazy! sync" +qa
 
 echo "NVIM: Install LSP with Mason Api. lua-language-server." | tee -a $HOME/setup.log
 # nvim --headless -c 'lua require("mason-registry").refresh()' \
