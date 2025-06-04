6729df96d30a6e7cd869977a70138aaf8a68fa97
mgallegoa <manuelgallego2003@yahoo.com>
2025-06-04 15:45:40 -0500

    Nvim : Upgrade linter (conform) and add linter for lua files with selene  (Mason).

--- .config/nvim/lua/plugins/linters.lua
+++ .config/nvim/lua/plugins/linters.lua
@@ -1,5 +1,6 @@
 return {
   "mfussenegger/nvim-lint",
+  -- commit = "b47cbb2",
   commit = "d698d3b",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
@@ -13,7 +14,15 @@ return {
       svelte = { "eslint_d" },
       htmlhint = { "htmlhint" },
       python = { "pylint" },
+      lua = { "selene" }, -- luacheck Require luarock and selene use Rust
     }
+
+    -- Custom configuration to recognize the configuration file
+    lint.linters.selene.args = {
+      "--config",
+      vim.fn.expand("~/.config/nvim/.selene.toml"),
+    }
+
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
     -- https://github.com/mfussenegger/nvim-lint/issues/482
     local get_clients = vim.lsp.get_clients or vim.lsp.get_active_clients
