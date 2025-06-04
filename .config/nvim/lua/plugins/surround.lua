628af0a3be11379de9c058fd129f3ee206bd9133
mgallegoa <manuelgallego2003@yahoo.com>
2025-06-04 15:59:52 -0500

    Nvim : Upgrade surround from v2.3.0 to v3.1.2

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,7 +1,7 @@
 return {
   "kylechui/nvim-surround",
   -- version = "*", -- Use for stability; omit to use `main` branch for the latest features
-  version = "^2.3.0",
+  version = "^3.1.2",
   event = "VeryLazy",
   config = function()
     require("nvim-surround").setup({
