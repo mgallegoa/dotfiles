1d637fd2532857ba501d592726f76c45c423e261
mgallegoa <manuelgallego2003@yahoo.com>
2025-06-05 09:56:49 -0500

    Nvim : Downgrade modicator, close the buffer using the spell list z=

--- .config/nvim/lua/plugins/modicator.lua
+++ .config/nvim/lua/plugins/modicator.lua
@@ -1,7 +1,9 @@
 -- This modicator (modify indicator) is for highlight the line number https://github.com/mawkler/modicator.nvim
 return {
   "mawkler/modicator.nvim",
-  commit = "45b6456",
+  -- commit = "45b6456", -- Latest but error for Nvim 0.11.2
+  commit = "1940afc",
+  enabled = true,
   dependencies = "catppuccin/nvim", -- Theme here
   init = function()
     -- These are required for Modicator to work
@@ -9,6 +11,7 @@ return {
     vim.o.number = true
     vim.o.termguicolors = true
   end,
+
   config = function()
     require("modicator").setup()
   end,
