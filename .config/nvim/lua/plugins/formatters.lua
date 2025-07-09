93903211a131b64543da02267c4f997a11b791db
mgallegoa <manuelgallego2003@yahoo.com>
2025-07-09 18:37:56 -0500

    Nvim : Add yml file tipe to validate formatter with prettier

--- .config/nvim/lua/plugins/formatters.lua
+++ .config/nvim/lua/plugins/formatters.lua
@@ -16,6 +16,7 @@ return {
         html = { "prettier" },
         json = { "prettier" },
         yaml = { "prettier" },
+        yml = { "prettier" },
         markdown = { "prettier" },
         graphql = { "prettier" },
         liquid = { "prettier" },
